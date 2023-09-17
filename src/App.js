fa083898d3a729c135cef72b9ff9bd9bf715d82a
SunitaMousavi <107063207+SuniElVi@users.noreply.github.com>
2023-09-17 19:30:21 +0330

    Added HTML
    Search input
    City and temperature header
    Weather description

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+
+
+        <div className="group">
+          <form>
+            <input type="Search" placeholder="Search" className="input" />
+          </form>
+        </div>
+
+        <h1>London</h1>
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+          alt="Scattered Clouds"
+        />
+        <h1>20</h1>
+        <h6>Friday, 15:00</h6>
+        <h6>Broken Clouds</h6>
+        <h6>Humidity: 92%</h6>
+        <h6>Wind: 3.13km/h</h6>
+      </div>
     </div>
   );
 }
-
-export default App;
