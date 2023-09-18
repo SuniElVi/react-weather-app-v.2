17039e1381ab743667375a4556e9cabaabdda8f9
SunitaMousavi <107063207+SuniElVi@users.noreply.github.com>
2023-09-18 19:49:37 +0330

    HTML
    Added the search button
    Styled the background
    Styled the weather details

--- src/App.js
+++ src/App.js
@@ -4,24 +4,48 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <form className="form">
+          <div className="row">
+            <div className="col-9">
+              <input type="Search" placeholder="Search" className="input" />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn w-70" />
+            </div>
+          </div>
+        </form>
 
-
-        <div className="group">
-          <form>
-            <input type="Search" placeholder="Search" className="input" />
-          </form>
+        <div className="row">
+          <div className="col-9">
+            <h1>London</h1>
+          </div>
+          <div className="col-1">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="Scattered Clouds"
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-1">
+            <h1>20</h1>
+          </div>
         </div>
 
-        <h1>London</h1>
-        <img
-          src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-          alt="Scattered Clouds"
-        />
-        <h1>20</h1>
-        <h6>Friday, 15:00</h6>
-        <h6>Broken Clouds</h6>
-        <h6>Humidity: 92%</h6>
-        <h6>Wind: 3.13km/h</h6>
+        <div className="row">
+          <div className="col-6 col-sm-3">
+            <h6>Friday, 15:00</h6>
+          </div>
+          <div className="col-6 col-sm-3">
+            <h6>Broken Clouds</h6>
+          </div>
+          <div class="w-100"></div>
+          <div className="col-6 col-sm-3">
+            <h6>Humidity: 92%</h6>
+          </div>
+          <div className="col-6 col-sm-3">
+            <h6>Wind: 3.13km/h</h6>
+          </div>
+        </div>
       </div>
     </div>
   );
